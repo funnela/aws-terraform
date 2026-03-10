bf000d1537f436fc8ece9ece21404005565551f8
Wiktor Latanowicz <wiktor@latanowicz.com>
2026-03-10 10:07:48 +0100

    fix db params

--- rds.tf
+++ rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_db_parameter_group" "default" {
-  name   = "funnela-${var.account}-db-pg14"
+  name   = "funnela-${var.account}-db-pg14-v1"
   family = "postgres14"
 
   parameter {
